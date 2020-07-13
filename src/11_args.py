e596fbb7c22f3fd8fcb63aa351a62870bc12d621
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-07-13 19:11:01 -0400

    complete assignment

--- src/11_args.py
+++ src/11_args.py
@@ -6,15 +6,15 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-def f1(arg1, arg2):
-    sum = arg1 + arg2
+def f1(args1, args2):
+    sum = args1 + args2
     return sum
     
 
 # YOUR CODE HERE
-print('\nargs: 1\n')
+print("\nArgs #1:")
 print(f1(1, 2))
-print('\n')
+print("\n")
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
@@ -22,32 +22,34 @@ print('\n')
 
 def f2(*args):
     return sum(args)
-    # return sum(args)
-    # my_sum = 0
-    # for arg in args:
-    #     my_sum += arg
-    # return my_sum
+    return sum(args)
+    my_sum = 0
+    for arg in args:
+        my_sum += arg
+    return my_sum
 
 # YOUR CODE HERE
 
-print('\n')
+print('\nArgs #2')
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
-print('\n')
-a = [7, 6, 5, 4]
+# a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
-
+print(f2(7, 6, 5, 4))    # Should print 22
+print('\n')
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
-
+# TODO: Need help with this
+def f3(int1, int2=1):
+    added = int1 + int2
+    return added
+# # YOUR CODE HERE
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -59,6 +61,11 @@ print(f3(8))     # Should print 9
 # key: baz, value: 12
 #
 # Note: Google "python keyword arguments".
+print('Args #4a:')
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f'Key: {key}, Value: {value}')
+
 
 # YOUR CODE HERE
 
@@ -66,17 +73,19 @@ print(f3(8))     # Should print 9
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
-
+print('\n')
+print('Args $4b:')
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
-
+print('\n')
+##I don't understand this
 d = {
     "monster": "goblin",
     "hp": 3
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
